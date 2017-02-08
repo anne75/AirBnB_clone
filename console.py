60e2156828d08ea6496d156676c2ab07fcc4adca
Incitatous <dcianberlin@gmail.com>
2017-02-07 16:19:49 -0800

    add EOF help quit

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+import sys
+
+
+class myPrompt(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """ Implements the quit command"""
+        raise SystemExit
+
+    def do_EOF(self, args):
+        """Implements EOF"""
+        raise EOFError
+
+if __name__ == "__main__":
+    myPrompt().cmdloop()
