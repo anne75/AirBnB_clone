3818272a9b878265d8574709d45202daba067479
Incitatous <dcianberlin@gmail.com>
2017-02-09 15:52:18 -0800

    printing class name properly

--- models/base_model.py
+++ models/base_model.py
@@ -2,20 +2,25 @@
 import datetime
 import uuid
 import json
-
+import models
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = uuid.uuid4()
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
+        if isinstance(args, dict):
+            self.__dict__ = args
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """UPDATE CLASS PART"""
-        return ("[{}] ({}) {}".format(self.__class__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_json(self):
         self.__dict__.update({"__class__": "BaseModel"})
