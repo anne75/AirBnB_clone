fd4847712c3274b9960fd078e0df172576bfa20b
Anne <anne.cognet@holbertonschool.com>
2017-03-15 19:41:37 +0000

    add 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,13 +11,13 @@ fi
 
 #set a path to install the web_static pages
 varpath='/data/web_static/releases/test/'
-mkdir -p "$varpath"
+sudo mkdir -p "$varpath"
 if [ ! -e "${varpath}"/index.html ]; then
-    echo "<html><h1>Test</h1>file index</html>" > "${varpath}/index.html"
+    echo "<html><h1>Test</h1>file index</html>" | sudo tee "${varpath}/index.html"
 fi
 
 #create a symbolic link
-ln -sf "$varpath" /data/web_static/current
+sudo ln -sf "$varpath" /data/web_static/current
 
 # adduser ubuntu for docker
 #update ownership of /data
@@ -25,7 +25,9 @@ sudo chown -R ubuntu:ubuntu /data/
 
 #update the nginx config file
 new_string="\n    #set the location of the static site\n    location /hbnb_static {\n        alias /data/web_static/current/;\n   }"
-if grep -q /etc/nginx/sites-enabled/default -e "hbnb_static" ; then
-    sed -i "50 i\ $new_string" /etc/nginx/sites-available/default
+grep -q /etc/nginx/sites-enabled/default -e "hbnb_static"
+check2=$?
+if [ "$check2" -eq 1 ] ; then
+    sudo sed -i "51 i\ $new_string" /etc/nginx/sites-available/default
 fi
 sudo service nginx restart
