ded1b574ae179f40e666cb116b89fd836b255ea8
Anne <anne.cognet@holbertonschool.com>
2017-03-15 18:50:37 +0000

    fix if cmd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -25,7 +25,7 @@ sudo chown -R ubuntu:ubuntu /data/
 
 #update the nginx config file
 new_string="\n    #set the location of the static site\n    location /hbnb_static {\n        alias /data/web_static/current/;\n   }"
-if [ grep -q /etc/nginx/sites-enabled/default -e "hbnb_static" ]; then
+if grep -q /etc/nginx/sites-enabled/default -e "hbnb_static" ; then
     sed -i "50 i\ $new_string" /etc/nginx/sites-available/default
 fi
 sudo service nginx restart
