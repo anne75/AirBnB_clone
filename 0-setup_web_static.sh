9b7958784d8a7261e31beae1d9c38c08dd632285
Anne <anne.cognet@holbertonschool.com>
2017-03-15 18:35:44 +0000

    static server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# bash script to set up the servers for the deployment of web_static
+sudo apt-get update
+
+#check if nginx is installed
+command -v nginx
+check1=$?
+if [ "$check1" -eq 1 ]; then
+    sudo apt-get -y install nginx
+fi
+
+#set a path to install the web_static pages
+varpath='/data/web_static/releases/test/'
+mkdir -p "$varpath"
+if [ ! -e "${varpath}"/index.html ]; then
+    echo "<html>Test file index</html>" > "${varpath}/index.html"
+fi
+
+#create a symbolic link
+ln -sf "$varpath" /data/web_static/current
+
+adduser ubuntu #for docker
+#update ownership of /data
+sudo chown -R ubuntu:ubuntu /data/
+
+#update the nginx config file
+new_string="\n    #set the location of the static site\n    location /hbnb_static {\n        alias /data/web_static/current/;\n   }"
+sed -i "50 i\ $new_string" /etc/nginx/sites-available/default
+sudo service nginx restart
